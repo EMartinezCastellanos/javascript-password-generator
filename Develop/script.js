e24a75c68ec280da98cf9dfabdec7250f6550cfd
ErnestoM2014 <1996ernestomartinez@gmail.com>
2022-04-23 23:06:51 -0700

    created array variables to show available characters to be used in the password

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,5 @@
 // Assignment code here
 
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -14,23 +13,29 @@ function detPasscrit() {
   }
   //    b lowercase
   var wantLow = window.confirm("Would you like to include lowercase letters. Select OK for yes or cancel for no");
-
   //     c uppercase
   var wantUpp = window.confirm("Would you like to include uppercase letters. Select OK for yes or cancel for no");
-
   //    d numbers 
   var wantNum = window.confirm("Would you like to include nubmers. Select OK for yes or cancel for no");
-
   //    Include special characters
   var wantSpec = window.confirm("Do you wish to include special characters. Select OK for yes or cancel for no");
-generatePassword(passLength,wantLow,wantNum,wantSpec,wantUpp);
+  generatePassword(passLength, wantLow, wantNum, wantSpec, wantUpp);
 
-return null;
+  return null;
 
 }
 
-function generatePassword(a,b,c,d,e) {
-  console.log(a,b,c,d,e);
+function generatePassword(a, b, c, d, e) {
+  console.log(a, b, c, d, e);
+  var numbers =[0,1,2,3,4,5,6,7,8,9];
+  var specialChar =['!','@','#','$','%','&','*','?']
+  var characterCodes = Array.from(Array(26)).map( (_, i) => i + 97);
+  var lowerLetts = characterCodes.map(code => String.fromCharCode(code));
+  var charCodestwo = Array.from(Array(26)).map( (_,i) => i + 65);
+  var upperLetts = charCodestwo.map(code => String.fromCharCode(code));
+
+  // for (let i=0; i < passLength, i++) {
+  // }
   //2 Validate the User input is correct 
 
   //3 Generate password according to selection 
@@ -45,6 +50,7 @@ function generatePassword(a,b,c,d,e) {
 
 // Write password to the #password input
 function writePassword() {
+  detPasscrit();
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -55,5 +61,3 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
-detPasscrit();
